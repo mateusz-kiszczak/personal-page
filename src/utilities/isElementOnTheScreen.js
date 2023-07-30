413c922cc9554c1ace17f99057713dec7573d7ad
mateusz-kiszczak <kiszczak.m@gmail.com>
2023-07-30 15:12:20 +0100

    isElementOnTheScreen - add new parameter to the function (ratio)

--- src/utilities/isElementOnTheScreen.js
+++ src/utilities/isElementOnTheScreen.js
@@ -1,6 +1,6 @@
-const isElementOnTheScreen = (screenHeight, elementTop) => {
+const isElementOnTheScreen = (screenHeight, elementTop, ratio = 0.5) => {
   // When element's top reach a specific point in the screen (1/2);
-  const breakingPoint = screenHeight * 0.5;
+  const breakingPoint = screenHeight * ratio;
 
   if (elementTop < breakingPoint) {
     return true;
