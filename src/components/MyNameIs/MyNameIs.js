1d8f47c100832166d99be1af10bc9cac03d4373a
Mateusz Kiszczak <55249936+mateusz-kiszczak@users.noreply.github.com>
2025-09-02 16:13:18 +0100

    Update content in MyNameIs.js

--- src/components/MyNameIs/MyNameIs.js
+++ src/components/MyNameIs/MyNameIs.js
@@ -22,7 +22,7 @@ const MyNameIs = () => {
 
   return (
     <section className="about-me__my-name-is default-container-width">
-      <h2>Hi, my name is Mateusz</h2>
+      <h2>Hi, I am Mateusz</h2>
       <section  className="about-me__photo-and-array">
         <h3 style={{display: "none"}}>Photo and array of skills</h3>
         <div className="about-me__photo--wrapper">
@@ -36,17 +36,19 @@ const MyNameIs = () => {
       </section>
       <section className="about-me__description">
         <h3 style={{display: "none"}}>Shortly about me</h3>
-        <p>Hi, my name is <span>Mateusz</span>, and I am a passionate programmer based in London. I started my journey with programming a couple of years ago, and I continue to develop my skills, mainly in web and app development.</p>
+        <p>I am a motivated Full-Stack Web Developer beginning an exciting career change. I have a strong background in self-directed learning and hands-on project experience.</p>
         <br/>
-        <p>I gain my knowledge from a whole different kinds of sources. My core skills I get from the <span>Codecademy</span> learning platform, official documentations and books. The recent book that helped me with the website's backend is "PHP & MySQL: Server-side Web Development" by <span>Jon Duckett</span>. While doing projects, I go much deeper into technologies with official documentation and sometimes with the help of developers' blogs or forums like StackOverflow.</p>
+        <p>I am skilled in several modern web technologies. On the front end, I create user-friendly and responsive websites using React, JavaScript, HTML, CSS, and Sass. I enjoy bringing designs to life and making sure users have a smooth experience.</p>
         <br/>
-        <p>Most of my projects were finalising a specific subject I had learned. I ensure they are challenging enough to get into the particular technology beyond the scope.</p>
+        <p>On the back end, I develop solid server-side logic and manage databases with Python/Django, PHP, and SQL. I know how to build RESTful APIs and handle data efficiently to support dynamic web applications.</p>
         <br/>
-        <p>I am proficient with <span>front-end development</span> and aspects of <span>UI/UX</span>. My mainly used languages include <span>HTML</span>, <span>CSS</span> and <span>JavaScript</span>. I know well framework, <span>ReactJS</span>, with libraries like <span>Router</span> or <span>Redux</span>. I also work effectively with the CSS preprocessor <span>Sass</span>.For the website developing I often use the <span>jQuery</span> and <span>Ajax</span>. I learned how to create wireframes and concept projects with Figma to make the creation process more efficient. I am familiar with basic graphic editing and creating using <span>Adobe Photoshop</span> and <span>Illustrator</span>.</p>
+        <p>My development work focuses on projects. I have designed, built, and launched full-stack applications like "Kingpiggy," a royalty-free image platform, and "Quizify," an interactive quiz maker. These projects demonstrate my ability to manage complex features from start to finish, including user authentication, image processing, and database integration.</p>
         <br/>
-        <p>As for the backend technologies, I use <span>PHP</span> for <span>web development</span>. I work with <span>MySQL</span> to manage the database. I have improved my skills as a <span>Full-stack developer</span>, taking care of projects from design to entire competition and maintenance.</p>
+        <p>I have backed these experiences with formal certifications, like the Front-End Engineer Career Path from Codecademy and the Web Developer program from HarvardX (edX). These courses strengthened my understanding of computer science basics and advanced web programming.</p>
         <br/>
-        <p>I am open to new technologies and always happy to extend the list of my technical skills.</p>
+        <p>I am really excited about cloud deployment and see a lot of potential in AWS. I am working on improving my skills in this area to help with scaling and managing web application infrastructures. My goal is to become skilled in deploying strong, high-performance solutions.</p>
+        <br/>
+        <p>I am looking for an entry-level Junior Full-Stack or Front-End Developer role in London. I am eager to use my growing skills and strong work ethic in a development team. I love learning and am ready to face new challenges in a professional environment.</p>
       </section>
     </section>
   );
